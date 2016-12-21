b18e1bf6e52b50c183236406c067aa7874b7b1b3
Continue7777 <pigcat7777@163.com>
2016-12-21 19:46:14 +0800

    Add files via upload
    修改了一下cutArticle的函数 网络上的全角半角转化还是不太好用。

--- chartBot/chat.py
+++ chartBot/chat.py
@@ -2,6 +2,7 @@
 from Tkinter import *
 from random import choice
 import tkMessageBox
+import jieba
 import time
 
 #定义变量
@@ -18,16 +19,24 @@ def sendMessage(event):
 	botTalk=choice(answer)
 	userTalk=E1.get()
 	#显示数据
-	text.insert(END,userName +"  "+ nowtime + "\n", 'green')
-	text.insert(END,userTalk + "\n")
-	text.insert(END,botName +"  "+ nowtime + "\n", 'green')
-	text.insert(END,botTalk + "\n")
+	textMessage.insert(END,userName +"  "+ nowtime + "\n", 'green')
+	textMessage.insert(END,userTalk + "\n")
+	textMessage.insert(END,botName +"  "+ nowtime + "\n", 'green')
+	textMessage.insert(END,botTalk + "\n")
+	#分词
+	seg_list = jieba.cut(userTalk)
+	str=""
+	for i in seg_list:
+		str+=i.encode("utf-8")+" "
+	print str
+	textCutJieba.insert(END,"jieba: "+str+"\n")
 	#清空输入框
 	E1.delete(0, END)
-	text.see(END)
+	textMessage.see(END)
+	textCutJieba.see(END)
 
 #创建容器1
-f1 = Frame(app,height = "100px")
+f1 = Frame(app,height = 100)
 f1.pack()
 #创建滑轮
 scrollbar = Scrollbar(f1)
@@ -35,11 +44,19 @@ scrollbar.pack( side = RIGHT, fill=Y )
 #添加容器2
 f2 = Frame(app,height = "30px")
 f2.pack()
+#添加容器3
+f3 = Frame(app,height=300)
+f3.pack()
+#添加分词结果窗口
+textCutJieba = Text(f3,height = "30px")
+textCutJieba.pack()
+LabelJieba = Label(f3,text="jieba")
+LabelJieba.pack()
 #添加聊天窗口
-text = Text(f1, yscrollcommand = scrollbar.set)
-text.tag_config('green', foreground='#008B00')
-text.pack()
-scrollbar.config(command=text.yview)
+textMessage = Text(f1, yscrollcommand = scrollbar.set)
+textMessage.tag_config('green', foreground='#008B00')
+textMessage.pack()
+scrollbar.config(command=textMessage.yview)
 #添加输入框
 E1 = Entry(f2, bd = 2, width = 40)
 E1.bind('<Return>', sendMessage)
